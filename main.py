49125194f71528c9ab86e75c1c9a9d88a2807d47
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2023-12-23 10:48:24 -0300

    configs

--- main.py
+++ main.py
@@ -28,15 +28,54 @@ CLOUD = [pygame.image.load(os.path.join("D:\dino_simulation\Assets\Other", "Clou
 
 BG = [pygame.image.load(os.path.join("D:\dino_simulation\Assets\Other", "Track.png"))]
 
+class Dinosaur:
+    X_POS = 80
+    Y_POS = 310
+
+    def __init__(self):
+        self.duck_img = DUCKING
+        self.run_img = RUNNING
+        self.jump_img = JUMPING
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+        
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dinp_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+    def update(self, userInput):
+        if self.dino_duck:
+            self.dino_duck()
+        if self.dino_run:
+            self.dino_run()
+        if self.dino_jump:
+            self.dino_jump()
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+
 def main():
-    run = Trueclock = pygame.time.Clock()
+    run = True
+    clock = pygame.time.Clock()
+    player = Dinosaur()
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+    SCREEN.fill((255,255,255))
+    userInput = pygame.key.get_pressed()
 
+    player.draw(SCREEN)
+    player.update(userInput)
 
+    clock.tick(30)
+    pygame.display.update()
 
 main()
